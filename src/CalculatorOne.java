0af2ab70a6a07b3a008d597b4918371bb54914a7
kfy4vgj <mmazumdar@ups.com>
2022-07-19 13:02:05 -0400

    first commit

---
+++ src/CalculatorOne.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CalculatorOne {
+	public static final int ERROR_CODE = 9999;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter operand 1: ");
+		int a = scanner.nextInt();
+		System.out.println("Please enter operand 2: ");
+		int b = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Please enter operator: ");
+		String operator = scanner.nextLine();
+
+		//int a = Integer.parseInt(args[1]);
+		//int b = Integer.parseInt(args[2]);
+		//String operator = args[0];
+
+		float result = 0;
+
+		switch (operator) {
+			case "+":
+				result = a + b;
+				break;
+			case "-":
+				result = a - b;
+				break;
+			case "*":
+				result = a * b;
+				break;
+			case "/":
+				result = (float) a / b;
+				break;
+			default:
+				result = ERROR_CODE;
+		}
+
+		System.out.println("[Calculator.main] result is: "+result);
+	}
+}
+
